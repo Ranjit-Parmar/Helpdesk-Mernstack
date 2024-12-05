f3e724cb90c6f27e602f0d87d160f86430888799
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-12-05 20:03:05 +0530

    add client url

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ cloudinary.config({
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.CLIENT_URL, 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
   credentials: true, 
 }));
